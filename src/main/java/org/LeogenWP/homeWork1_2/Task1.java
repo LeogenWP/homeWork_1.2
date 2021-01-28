8ee71dec9e01dd289e3717c6c69d73b3b8f509ed
Gena Pankratov <gpankratov95@mail.ru>
2021-01-28 21:31:29 +0300

    add HashMap

--- src/main/java/org/LeogenWP/homeWork1_2/Task1.java
+++ src/main/java/org/LeogenWP/homeWork1_2/Task1.java
@@ -1,9 +1,6 @@
 package org.LeogenWP.homeWork1_2;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -18,38 +15,29 @@ public class Task1 {
     }
 
     private int[] arrayWithTermIndex (int[] array, int digit){
-        int[] arrayWithIndex;
-        List<Integer> termDigits = new LinkedList();
-        int tmpDigit = digit;
-        int digitPosition = 0;
-
-        Arrays.sort(array);
-        for(digitPosition = 0; digitPosition < array.length; digitPosition++){
-            if(digit < array[digitPosition]){
-                break;
+        int[] arrayWithIndex = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < array.length; i++){
+            map.put(array[i],digit - array[i]);
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(map.containsKey(entry.getValue())){
+                arrayWithIndex[0] = getArrayIndex(entry.getValue(), array);
+                arrayWithIndex[1] = getArrayIndex(entry.getKey(),array);
+                return arrayWithIndex;
             }
         }
-        while(digitPosition  > 0){
-          for(int i = digitPosition - 1; i > 0; i--){
-              if(tmpDigit >= array[i]){
-                  tmpDigit -= array[i];
-                  termDigits.add(i);
-              }
-              if (tmpDigit == 0){
-                  arrayWithIndex = new int[termDigits.size()];
-                  int pointer = arrayWithIndex.length;
-                  for (Integer k : termDigits){
-                      arrayWithIndex[pointer -1] = (int) k;
-                      pointer--;
-                  }
-                  return arrayWithIndex;
-              }
-          }
-            termDigits.clear();
-            tmpDigit = digit;
-            digitPosition--;
-        }
-        arrayWithIndex = new int[0];
         return arrayWithIndex;
+    }
+
+    private int getArrayIndex (Integer integer, int[] array){
+        int index = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(integer == array[i]){
+                index = i;
+                return index;
+            }
+        }
+        return index;
     }
 }
